6bad72da6b0cab41b959bd6499e65b790f26995e
Corey ODonnell <rcoreyodonnell@gmail.com>
2015-06-15 12:26:51 -0400

    Added required packages

---
+++ public/src/factories.js
@@ -0,0 +1,7 @@
+angular.module('ContactsApp')
+    .factory('Contact', function($resource) {
+        return $resource('/api/contact/:id', { id: '@id' }, {
+            'update': { method: 'PUT' }
+        });
+    });
+
