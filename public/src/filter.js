23a22dfafec9605bcb1d3440272b65892304dc25
Corey ODonnell <rcoreyodonnell@gmail.com>
2015-06-16 08:10:31 -0400

    Form Field Finished

--- public/src/filter.js
+++ public/src/filter.js
@@ -3,5 +3,23 @@ angular.module('ContactsApp')
         return function(input) {
             input = input.replace(/([A-Z])/g, ' $1');
             return input[0].toUpperCase() + input.slice(1);
-        }
+        };
     })
+    .filter('camelCase', function() {
+        return function(input) {
+            return input.toLowerCase().replace(/ (\w)/g, function(match, letter) {
+                return letter.toUpperCase();
+            });
+        };
+    })
+    .filter('keyFilter', function () {
+        return function (obj, query) {
+            var result = {};
+            angular.forEach(obj, function(val, key) {
+                if(key !== query) {
+                    result[key] = val;
+                }
+            });
+            return result;
+        };
+    });
